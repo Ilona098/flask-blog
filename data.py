5c3931a54e6dac07f47c53d7ad42ed15734f4e0f
Ilona Lange <ilona.lange098@gmail.com>
2019-06-19 16:53:03 +0100

    simple flask blog

---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit diam ac convallis accumsan. Nullam lectus dui, tempus a tellus sit amet, malesuada euismod lectus',
+            'author': 'J. Doe',
+            'create_date': '09-03-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit diam ac convallis accumsan. Nullam lectus dui, tempus a tellus sit amet, malesuada euismod lectus',
+            'author': 'Ilona Lange',
+            'create_date': '09-03-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit diam ac convallis accumsan. Nullam lectus dui, tempus a tellus sit amet, malesuada euismod lectus',
+            'author': 'M. Mildner',
+            'create_date': '09-03-2019'
+        }
+
+
+    ]
+    return articles
